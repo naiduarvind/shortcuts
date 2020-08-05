f0c2086aef9caecf58f736f052f15ac228f464f9
Arvind Naidu <arvind@thebility.engineer>
2020-08-05 20:10:22 +0800

    feat(shortcuts): introduce vuedarkmode for dark mode
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueDarkMode from "vuedarkmode";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -10,3 +11,7 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+Vue.use(VueDarkMode, {
+  theme: "dark",
+});
