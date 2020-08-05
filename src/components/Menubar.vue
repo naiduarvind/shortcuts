955251c84c79c7e8b1e6ece32b026d161d2ed632
Arvind Naidu <arvind@thebility.engineer>
2020-08-05 19:26:02 +0800

    feat(shortcuts): image and favicon consistency
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- src/components/Menubar.vue
+++ src/components/Menubar.vue
@@ -91,7 +91,10 @@ p {
   <div class="menubar" v-if="showNav">
     <div class="d-flex flex-column">
       <h1 class="menu__title d-flex flex-row align-items-center">
+        <img src=/src/assets/favicon.png alt="T.(*)b.E Logo">
+        <!-- 
         <i class="em em-keyboard"></i>
+        -->
         <span class="pl-10">T.(*)b.E Shortcuts</span>
       </h1>
 
